6bb0071b5c4f376fbc325f6402ff018aab2ad5ec
Your Name <you@example.com>
2024-06-21 20:38:37 +0200

    Code angepasst

--- Frontend/script2.js
+++ Frontend/script2.js
@@ -1,17 +1,17 @@
 const params1 = new URLSearchParams(window.location.search);
-const id1 = params.get('id1');
+const id1 = params1.get('id1');
 console.log(id1);
 let genrestring= localStorage.getItem(id1)	
 let genrefile= JSON.parse(genrestring)	
-console.log (genre1)
-let buchtitelseite2= document.getElementById("genresContainer")
-buchtitelseite2.textContent= genre1
+console.log (genrefile)
+//let buchtitelseite2= document.getElementById("genresContainer")
+//buchtitelseite2.textContent= genre1
 
 
 // Dokumenttitel= Buchtitel
-        document.title = genre1;
+        document.title = genrefile.genre;
 
 
 // Seiten-Titel
         let ueberschriftbuch = document.getElementById ("ueberschriftbuch")
-        ueberschriftbuch.textContent = genre1
+        ueberschriftbuch.textContent = genrefile.genre
